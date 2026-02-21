57eb58c06539203cfc9a8708adb5d2f529bb7ef7
AnonymousAN21 <reeve.tangel@gmail.com>
2026-02-22 00:59:58 +0800

    queue

--- main.c
+++ main.c
@@ -3,33 +3,7 @@
 #include "queue.h"
 #include <stdio.h>
 #include <time.h>
-#define N 5
 
-void BFS(int graph[N][N], int start){
-    queue q;
-    Queue(&q, sizeof(int), N);
-
-    bool visited[N] = {false};
-
-    q.enqueue(&q, &start);
-    visited[start] = true;
-
-    while(!q.isEmpty(&q)){
-
-        int current = *(int*)q.dequeue(&q);
-
-        printf("%d ", current);
-
-        for(int i = 0; i < N; i++){
-            if(graph[current][i] == 1 && !visited[i]){
-                visited[i] = true;
-                q.enqueue(&q, &i);
-            }
-        }
-    }
-
-    q.clear(&q);
-}
 int compareInt(const void* a, const void* b) {
     int x = *(int*)a;
     int y = *(int*)b;
@@ -41,15 +15,4 @@ int compareInt(const void* a, const void* b) {
 
 int main(){
 
-    int graph[5][5] = {
-        {0,1,0,1,0},
-        {1,0,1,1,1},
-        {0,1,0,0,0},
-        {1,1,0,0,1},
-        {0,1,0,1,0}
-    };
-
-    BFS(graph, 0);
-
-    return 0;
 }
